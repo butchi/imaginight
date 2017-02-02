7234198a90e19b62cc5b4ffdcdc20456ec84fb9d
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2017-02-03 00:47:17 +0900

    add characters

--- src/js/config/character-list.js
+++ src/js/config/character-list.js
@@ -19,6 +19,18 @@ const CharacterLi = [
     },
   },
   {
+    name: "イマジナイト",
+    hp: [3,2],
+    job: "Imagiknight",
+    special: {
+      name: "斜め切り",
+      func: normalFunc({
+        operator: '-',
+        power: [2,2],
+      }),
+    },
+  },
+  {
     name: "ダークナイト",
     hp: [-4,0],
     job: "Dark Knight",
@@ -26,19 +38,43 @@ const CharacterLi = [
       name: "暗黒剣",
       func: normalFunc({
         operator: '-',
-        power: [2,0],
+        power: [3,0],
       }),
     },
   },
   {
     name: "魔道士",
-    hp: [0,3],
+    hp: [3,0],
     job: "Magician",
     special: {
       name: "属性変化",
       func: normalFunc({
         operator: '*',
         power: [0,1],
+      }),
+    },
+  },
+  {
+    name: "暗黒魔道士",
+    hp: [-3,0],
+    job: "Magician",
+    special: {
+      name: "属性逆変化",
+      func: normalFunc({
+        operator: '*',
+        power: [0,-1],
+      }),
+    },
+  },
+  {
+    name: "道化師",
+    hp: [2,1],
+    job: "Joker",
+    special: {
+      name: "変身",
+      func: normalFunc({
+        operator: '*',
+        power: [1,2],
       }),
     },
   },
@@ -68,6 +104,42 @@ const CharacterLi = [
       }),
       desc: '戦闘不能のHPを(4,0)にする',
     }
+  },
+  {
+    name: "ゴースト",
+    hp: [0,-3],
+    job: "Ghost",
+    special: {
+      name: "怨念",
+      func: normalFunc({
+        operator: '-',
+        power: [2,0],
+      }),
+    },
+  },
+  {
+    name: "悪魔",
+    hp: [0,-4],
+    job: "Devil",
+    special: {
+      name: "投獄",
+      func: normalFunc({
+        operator: '-',
+        power: [2,0],
+      }),
+    },
+  },
+  {
+    name: "大天使",
+    hp: [0,4],
+    job: "Angel",
+    special: {
+      name: "祝福",
+      func: normalFunc({
+        operator: '+',
+        power: [2,0],
+      }),
+    },
   },
 ];
 
