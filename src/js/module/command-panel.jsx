2b55b0f27992393d2b208028bad14584f650068c
IWABUCHI Yu(u)ki <k38647+reg@gmail.com>
2017-02-02 18:34:02 +0900

    fix style

--- src/js/module/command-panel.jsx
+++ src/js/module/command-panel.jsx
@@ -30,7 +30,7 @@ export default class CommandPanel extends React.Component {
 
     Object.keys(this.props.ability).forEach((key) => {
       let command = this.props.ability[key];
-      commandElmArr.push(<label key={index + 1}><input type="radio" ref="command" value={key} checked={this.state.command.id === key} onChange={() => this.handleChange(key)} /> {command.name}</label>);
+      commandElmArr.push(<label className={key} key={index + 1}><input type="radio" ref="command" value={key} checked={this.state.command.id === key} onChange={() => this.handleChange(key)} /> {command.name}</label>);
 
       index++;
     });
@@ -38,8 +38,8 @@ export default class CommandPanel extends React.Component {
     return (
       <div className="command-panel">
         {commandElmArr}
-        <label key={0}><input type="radio" ref="command" value="special" checked={this.state.command.id === 'special'} onChange={() => this.handleChange('special')} /> {this.props.special.name}</label>
-        <div>{this.props.special.name}: {this.props.special.desc}</div>
+        <label className="special" key={0}><input type="radio" ref="command" value="special" checked={this.state.command.id === 'special'} onChange={() => this.handleChange('special')} /> {this.props.special.name}</label>
+        <div className="desc">{this.props.special.name}: {this.props.special.desc}</div>
       </div>
     );
   }
